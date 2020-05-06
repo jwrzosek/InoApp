54c640c5f31a878bca63aa442d8a9ca4374fc051
Jakub Wrzosek <wrzosek.jakub2@gmail.com>
2020-05-07 00:51:14 +0200

    Enable data binding in build.gradle (app)

--- app/src/main/java/com/example/inoapp/MainActivity.kt
+++ app/src/main/java/com/example/inoapp/MainActivity.kt
@@ -68,12 +68,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             R.id.nav_home -> {
                 // Handle the camera action
             }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
             R.id.nav_tools -> {
 
             }
