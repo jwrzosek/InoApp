043c1e6845fb04f7e41949a9de3fafc86ebdfc92
Jakub Wrzosek <wrzosek.jakub2@gmail.com>
2020-05-11 15:56:06 +0200

    AddNewPoint Fragment and Layout added.

--- app/src/main/java/com/example/inoapp/addnewtrip/AddNewPointFragment.kt
+++ app/src/main/java/com/example/inoapp/addnewtrip/AddNewPointFragment.kt
@@ -6,29 +6,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import com.example.inoapp.R
+import com.example.inoapp.databinding.FragmentAddNewPointBinding
 import com.example.inoapp.databinding.FragmentAddNewTripBinding
 
 /**
  * A simple [Fragment] subclass.
  *
  */
-class AddNewTripFragment : Fragment() {
+class AddNewPointFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        val binding = DataBindingUtil.inflate<FragmentAddNewTripBinding>(inflater,
-            R.layout.fragment_add_new_trip, container,false)
+        val binding = DataBindingUtil.inflate<FragmentAddNewPointBinding>(inflater,
+            R.layout.fragment_add_new_point, container,false)
 
         //The complete onClickListener with Navigation
-        binding.addNewTripAddNewPoint.setOnClickListener { view : View ->
-            view.findNavController().navigate(R.id.action_addNewTripFragment_to_addNewPointFragment)
-        }
+        binding.addNewPointBackButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_addNewPointFragment_to_addNewTripFragment)
+         }
 
         return binding.root
     }
-
 
 }
